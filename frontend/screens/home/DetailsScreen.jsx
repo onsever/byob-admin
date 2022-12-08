63fcb59dce3fac621d2aac0eac8b65dbed571050
Onurcan Sever <onsever@icloud.com>
2022-12-08 09:36:14 -0500

    re-implementation of the screens

--- frontend/screens/home/DetailsScreen.jsx
+++ frontend/screens/home/DetailsScreen.jsx
@@ -1,15 +1,39 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import { useFetch } from "../../hooks/useFetch";
 import tw from "twrnc";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import CheckBox from "../../components/CheckBox";
 
 export default function DetailsScreen({ route, navigation }) {
   const { item } = route.params;
   const { loaded, loading, error, fetch, result } = useFetch();
   const [data, setData] = React.useState(null);
   const [infoClicked, setInfoClicked] = React.useState(false);
+  const [totalPrice, setTotalPrice] = React.useState(0);
+  const [grandTotal, setGrandTotal] = React.useState(0);
+  const [gratuityClicked, setGratuityClicked] = React.useState(false);
+
+  const dateParser = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = d.getMonth() + 1;
+    const day = d.getDate();
+    return `${day}/${month}/${year}`;
+  };
+
+  const toTwoDecimal = (num) => {
+    return num.toFixed(2);
+  };
+
+  const gstCalculator = (price) => {
+    return price * 0.13;
+  };
+
+  const grandTotalCalculator = (price) => {
+    return +toTwoDecimal(price) + +toTwoDecimal(gstCalculator(price));
+  };
 
   React.useEffect(() => {
     fetch("table/" + item);
@@ -25,15 +49,25 @@ export default function DetailsScreen({ route, navigation }) {
     if (loaded) {
       if (error) {
         console.log(error);
+        Alert.alert("Error", error.data);
+        navigation.goBack();
       }
       if (result) {
         setData(result);
+        setTotalPrice(
+          result.order.drinkOrder.reduce((acc, cur) => {
+            return acc + cur.price * cur.quantity;
+          }, 0) +
+            result.order.order[0].order.reduce((acc, cur) => {
+              return acc + cur.price * cur.quantity;
+            }, 0)
+        );
       }
     }
   }, [loaded]);
 
   return (
-    <SafeAreaView style={tw`w-full h-full`}>
+    <View style={tw`w-full h-full`}>
       {loading && !loaded ? (
         <ActivityIndicator
           size="large"
@@ -41,23 +75,142 @@ export default function DetailsScreen({ route, navigation }) {
           style={tw`w-full h-full`}
         />
       ) : (
-        <>
-          <View style={tw`w-full h-full`}>
-            <View style={tw`flex-row items-center justify-evenly`}>
-              <Text style={tw`text-center font-bold`}>
-                Table For {data?.user.firstName + " " + data?.user.lastName}
-              </Text>
-              <Ionicons
-                name="information-circle"
-                size={24}
-                color="blue"
-                onPress={() => setInfoClicked(!infoClicked)}
-              />
-            </View>
-            {data?.order && <Text>There is order</Text>}
+        <View style={tw`w-full h-full items-center`}>
+          <View style={tw`flex-row items-center my-2`}>
+            <Text style={tw`text-center font-bold text-xl mr-4`}>
+              Customer: {data?.user.firstName + " " + data?.user.lastName}
+            </Text>
+            <Ionicons
+              name="information-circle"
+              size={30}
+              color="blue"
+              onPress={() => setInfoClicked(!infoClicked)}
+            />
           </View>
-        </>
+          {infoClicked && (
+            <View style={tw`w-[90%] bg-slate-200/40 rounded-lg px-2 py-4`}>
+              <Text style={tw`text-center text-lg font-semibold mb-1`}>
+                Customer Information
+              </Text>
+              <View style={tw`w-full h-[1px] bg-black`}></View>
+              <View style={tw`flex-row justify-between mt-2`}>
+                <Text style={tw`font-semibold`}>Email Address:</Text>
+                <Text>{data?.user.email}</Text>
+              </View>
+              <View style={tw`flex-row justify-between mt-2`}>
+                <Text style={tw`font-semibold`}>Phone Number:</Text>
+                <Text>{data?.user.phone}</Text>
+              </View>
+              <View style={tw`flex-row justify-between mt-2`}>
+                <Text style={tw`font-semibold`}>Date of Birth:</Text>
+                <Text>{dateParser(data?.user.dob)}</Text>
+              </View>
+            </View>
+          )}
+          {data?.order ? (
+            <View style={tw`w-[90%] items-center justify-center`}>
+              <View style={tw`w-full flex-row items-center justify-between`}>
+                <Text style={tw`font-semibold text-lg`}>Orders</Text>
+                <Text style={tw`underline`}>
+                  {data?.order.isComplete ? "Completed" : "Not Completed"}
+                </Text>
+              </View>
+              <View style={tw`w-full h-[1px] bg-black`}></View>
+              <View style={tw`w-full flex-row justify-between mt-2`}>
+                <Text style={tw`font-semibold`}>Order ID:</Text>
+                <Text>{data?.order._id}</Text>
+              </View>
+              <View style={tw`w-full flex-row justify-between mt-2`}>
+                <Text style={tw`font-semibold`}>Total Number of Orders:</Text>
+                <Text>{data?.order.__v} orders</Text>
+              </View>
+              <View style={tw`w-full flex-row justify-between mt-6`}>
+                <Text style={tw`font-semibold`}>Name</Text>
+                <View style={tw`flex-row items-center`}>
+                  <Text style={tw`mr-4`}>Price ($)</Text>
+                  <Text style={tw``}>Quantity (n)</Text>
+                </View>
+              </View>
+              <View style={tw`w-full h-[1px] bg-black`}></View>
+              {/* Drink Orders Table */}
+              {data?.order.drinkOrder.map((item, index) => {
+                return (
+                  <View
+                    key={index}
+                    style={tw`w-full flex-row justify-between mt-2`}
+                  >
+                    <Text style={tw`font-semibold`}>Drink {index + 1}</Text>
+                    <View style={tw`flex-row items-center`}>
+                      <Text style={tw`mr-2`}>{item.price}$</Text>
+                      <Text>{item.quantity}</Text>
+                    </View>
+                  </View>
+                );
+              })}
+              {/* Food Orders Table */}
+              {data?.order.order &&
+                data?.order.order.map((item, i) => {
+                  return item.order.map((item, index) => {
+                    return (
+                      <View
+                        key={index}
+                        style={tw`w-full flex-row justify-between mt-2`}
+                      >
+                        <Text style={tw`font-semibold`}>Food {index + 1}</Text>
+                        <View style={tw`flex-row items-center`}>
+                          <Text style={tw`mr-2`}>{item.price}$</Text>
+                          <Text>{item.quantity}</Text>
+                        </View>
+                      </View>
+                    );
+                  });
+                })}
+              {/* Calculation */}
+              <View style={tw`w-full items-start mt-2 border-b`}>
+                <Ionicons name="add" size={20} color="black" />
+              </View>
+              <View
+                style={tw`w-full flex-row items-center justify-between mt-2`}
+              >
+                <Text style={tw`font-semibold`}>Subtotal</Text>
+                <Text style={tw``}>{toTwoDecimal(totalPrice)}$</Text>
+              </View>
+              <View
+                style={tw`w-full flex-row items-center justify-between mt-2`}
+              >
+                <Text style={tw`font-semibold`}>GST (13%)</Text>
+                <Text style={tw``}>
+                  {toTwoDecimal(gstCalculator(totalPrice))}$
+                </Text>
+              </View>
+              <View
+                style={tw`w-full flex-row items-center justify-between mt-2`}
+              >
+                <Text style={tw`font-semibold`}>Grand Total</Text>
+                <Text style={tw``}>{grandTotalCalculator(totalPrice)}$</Text>
+              </View>
+              {/* Gratuity */}
+              <View
+                style={tw`w-full flex-row items-center justify-between mt-2`}
+              >
+                <Text style={tw`font-semibold`}>Gratuity</Text>
+                <CheckBox
+                  onAction={(checked, setChecked) => {
+                    setChecked(!checked);
+                    setGratuityClicked(checked);
+                  }}
+                />
+              </View>
+            </View>
+          ) : (
+            <View style={tw`w-[90%] h-2/3 items-center justify-center`}>
+              <Text style={tw`font-semibold text-lg`}>
+                There is no order for this table yet.
+              </Text>
+            </View>
+          )}
+        </View>
       )}
-    </SafeAreaView>
+    </View>
   );
 }
