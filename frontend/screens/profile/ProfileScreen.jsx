edeffa13848a0b6251fc967890974228d4b1963c
Vishal <vishalk0220@gmail.com>
2022-12-12 20:45:56 -0500

    admin navigation

--- frontend/screens/profile/ProfileScreen.jsx
+++ frontend/screens/profile/ProfileScreen.jsx
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Header from "../../components/header";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/features/authSlice";
+
 export default function ProfileScreen() {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView>
       <Text>Hey</Text>
+      <TouchableOpacity style={{ backgroundColor: "grey", padding: 10, margin: 10 }}
+        onPress={() => {
+          dispatch(logout())
+        }}
+      >
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
